883c362ac9b04011bed044cfe2928b7e9d07720d
Alex Berardi <AlexmBerardi@gmail.com>
2017-02-04 13:13:54 -0500

    Adding foundation to nav, examples and main component.

--- app/Components/About.jsx
+++ app/Components/About.jsx
@@ -2,7 +2,10 @@ var React = require('react');
 
 var About = (props) => {
   return (
-    <p>About</p>
+    <div>
+    	<h3>About</h3>
+    	<p>Welcome to the About page!</p>
+    </div>
   )
 };
 
