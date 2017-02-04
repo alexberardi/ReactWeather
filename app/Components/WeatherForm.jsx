0ef27cedfa5ebb3954cfa8e17bb1a23d01187b88
Alex Berardi <AlexmBerardi@gmail.com>
2017-02-04 10:21:53 -0500

    Making fixes for heroku

--- app/Components/WeatherForm.jsx
+++ app/Components/WeatherForm.jsx
@@ -1,27 +1,26 @@
 var React = require('react');
 
 var WeatherForm = React.createClass({
-    onFormSubmit: function(e) {
-        e.preventDefault();
+  onFormSubmit: function (e) {
+    e.preventDefault();
 
-        var location = this.refs.location.value;
-        if (location.length > 0) {
-            this.refs.location.value = '';
-            this.props.onSearch(location);
-        }
-    },
-    render: function() {
-        return (
-            <form onSubmit={this.onFormSubmit}>
-                <div>
-                    <input type="text" ref="location"/>
-                </div>
-                <div>
-                    <button>Get Weather</button>
-                </div>
-            </form>
-        );
+    var location = this.refs.location.value;
+
+    if (location.length > 0) {
+      this.refs.location.value = '';
+      this.props.onSearch(location);
     }
+  },
+  render: function () {
+    return (
+        <div>
+          <form onSubmit={this.onFormSubmit}>
+            <input type="text" ref="location"/>
+            <button>Get Weather</button>
+          </form>
+        </div>
+    );
+  }
 });
 
 module.exports = WeatherForm;
--- example-promise.js
+++ example-promise.js
@@ -1,46 +1,50 @@
 // function getTempCallback (location, callback) {
-//     callback(undefined, 78);
-//     callback('City not found');
+//   callback(undefined, 78);
+//   callback('City not found');
 // }
 //
-// getTempCallback('Philadelphia', function(err, temp){
-//     if(err) {
-//         console.log('error', err);
-//     } else {
-//         console.log('success', temp);
-//     }
+// getTempCallback('Philadelphia', function (err, temp) {
+//   if (err) {
+//     console.log('error', err);
+//   } else {
+//     console.log('success', temp)
+//   }
 // });
 //
-// function getTempPromise(location) {
-//     return new Promise(function (resolve, reject) {
-//         setTimeout(function(){
-//             resolve(79);
-//             reject('City not found');
-//         }, 1000);
-//     });
+// function getTempPromise (location) {
+//   return new Promise(function (resolve, reject) {
+//     setTimeout(function () {
+//       resolve(79);
+//       reject('City not found');
+//     }, 1000);
+//   });
 // }
 //
 // getTempPromise('Philadelphia').then(function (temp) {
-//    console.log('promise success ', temp);
-// }, function(err){
-//     console.log('promise error ', err);
+//   console.log('promise success', temp);
+// }, function (err) {
+//   console.log('promise error', err);
 // });
 
-
-//challenge area
-// 6d3cc3a632aaa6c93edd758ea9f36de9
-function addPromise(a, b){
-    return new Promise (function(resolve, reject){
-        if (typeof a === 'number' && typeof b === 'number'){
-            resolve(a + b);
-        } else {
-            reject('invalid numbers');
-        }
-    })
+// Challenge Area
+function addPromise (a, b) {
+  return new Promise(function (resolve, reject) {
+    if (typeof a === 'number' && typeof b === 'number') {
+      resolve(a + b);
+    } else {
+      reject('A & b need to be numbers');
+    }
+  });
 }
 
-addPromise(1, 2).then(function(sum) {
-    console.log('success', sum);
-}, function(error) {
-    console.log(error);
-})
+addPromise(2, 3).then(function (sum) {
+  console.log('success', sum);
+}, function (err) {
+  console.log('error', err);
+});
+
+addPromise('andrew', 9).then(function (sum) {
+  console.log('this should not show up');
+}, function (err) {
+  console.log('This should appear', err);
+});
